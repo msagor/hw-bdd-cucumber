51324a5f89405c7899f22a7d4c572ed3c2f882e7
EC2 Default User <ec2-user@ip-172-31-6-167.us-east-2.compute.internal>
2021-03-03 21:21:38 +0000

    part 1 testing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented" #todo
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented" #todo
+  page.body.should =~ /#{e1}.*#{e2}/m
+  #fail "Unimplemented" #todo
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +31,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented" #todo
+  rating_list.split(', ').each do |rating|
+    if uncheck.nil?
+      step "I check \"ratings_#{rating}\""
+    else
+      step "I uncheck \"ratings_#{rating}\""
+    end
+  end
+  #fail "Unimplemented" #todo
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented" #todo
+  Movie.all() do |row|
+    title = row[:title]
+    step "I should see \"#{title}\""
+  end
+  #fail "Unimplemented" #todo
 end
